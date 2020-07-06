2ddb0cf96fbd528546a98ac9e9faf62f7024695d
Soumya Kanthi <soumya@e9ine.com>
2020-07-06 12:41:53 +0530

    fix: exports in index.js

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -137,8 +137,7 @@ export default {
 
 
         },
-        handleClick(event) { 
-            debugger;
+        handleClick() { 
         }
     },
     created () {
--- src/index.js
+++ src/index.js
@@ -3,24 +3,30 @@
 export {fromArray, download, escape} from './utils/CSV';
 export {sortArrayByProperty, checkValidHex} from './utils/CommonUtils';
 
-export { Button } from './components/Button';
-export { FormFor } from './components/field-for/FormFor';
-export { SubFormFor } from './components/field-for/SubFormFor';
-export { FieldFor } from './components/field-for/FieldFor';
-export { AddressFinder } from './components/AddressFinder';
 export { Avatar } from './components/Avatar';
+export { AvatarGroup } from './components/AvatarGroup';
+export { AvatarInfo } from './components/AvatarInfo';
+export { AddressFinder } from './components/address-finders';
+export { Badge } from './components/Badge';
+export { Breadcrumbs } from './components/Breadcrumbs';
+export { Button } from './components/Button';
 export { Card } from './components/Card';
+export { Chart } from './components/charts';
 export { Chip } from './components/Chip';
 export { ColorPicker } from './components/ColorPicker';
-export { ImageUpload } from './components/ImageUpload';
-export { Loader } from './components/Loader';
+export { DatePicker } from './components/DatePicker';
+export { Overlay } from './components/Overlay';
+export { Panel } from './components/Panel';
 export { ProgressBar } from './components/ProgressBar';
 export { Range } from './components/Range';
-export { Ratings } from './components/Ratings';
 export { Shimmer } from './components/Shimmer';
+export { SideBar } from './components/SideBar';
 export { Stepper } from './components/Stepper';
+export { Table } from './components/Table';
+export { Tabs } from './components/Tabs';
 export { TimePicker } from './components/TimePicker';
 export { Tooltip } from './components/Tooltip';
+export { TopBar } from './components/TopBar';
 
 // export directives from ./directives
 export { VClickOutside } from './directives/v-click-outside';
@@ -29,8 +35,9 @@ export { VDebounce } from './directives/v-debounce';
 export {ModalMixin} from './mixins/modal';
 
 // export layouts from ./layouts
-export { ModuleBaseLayout } from './layouts/ModuleBaseLayout';
 export { ModuleItemLayout } from './layouts/ModuleItemLayout';
 
 // export views from ./views
 export { LoginVariation1 } from './views/login/variation-1/Component';
+export { LayoutVariation1 } from './views/layout/variation-1/Component';
+export { LayoutVariation2 } from './views/layout/variation-2/Component';
